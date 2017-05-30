ea11020e9a5bf4189ad543b74d8cb24e86215652
DonChef660 <fehmi@raqipi.ch>
2017-05-30 15:52:01 +0200

    Initial Upload

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+/************************************************************************
+ * Extension Manager/Repository config file for ext "language_handling".
+ ************************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'language_handling',
+    'description' => 'language_handling',
+    'category' => 'extension',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.99.99'
+            
+        ),
+        'conflicts' => array(
+        ),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Fehmi Raqipi',
+    'author_email' => 'fehmi.raqipi@terminal8.ch',
+    'author_company' => 'Terminal8',
+    'version' => '1.0.0',
+);
